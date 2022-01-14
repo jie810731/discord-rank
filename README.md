121347d5e5a02483cd9c872d2c7b9147f28067cf
JosephTang <josephtang@starlux-airlines.com>
2022-01-14 17:07:35 +0800

    pokemon

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # My Discord Rank
+
 肝白名單
 
 ## how to build
@@ -8,20 +9,39 @@ docker build -t my-discord-rank . --no-cache
 ## how to run
 
 ### for product
+
 docker run -i -t -d
- -e Token={{auth_token}}
- -e Channel_Id={{channel_id}}
- --name {{container_name}} my-discord-rank
+-e Token={{auth_token}}
+-e Channel_Id={{channel_id}}
+--name {{container_name}} my-discord-rank
 
 ### for develop
-docker run -i -t --rm -v "$PWD":/usr/src/myapp -w /usr/src/myapp 
- -e Token={{auth_token}}
- -e Channel_Id={{channel_id}}
- --name test my-discord-rank
+
+docker run -i -t --rm -v "$PWD":/usr/src/myapp -w /usr/src/myapp
+-e Token={{auth_token}}
+-e Channel_Id={{channel_id}}
+--name test my-discord-rank
+
 ## eagle
+
 ### build
+
 docker build -f EagleDockerfile -t eagle-chat .
+
 ### run
+
 docker run -i -t -d \
  -e Token={{token}} \
  --name {{container_name}} eagle-chat
+
+## pokemon
+
+### build
+
+docker build -f PokemonDockerfile -t pokemon .
+
+### run
+
+docker run -i -t \
+ -e Token={{token}} \
+ --name {{container_name}} pokemon
---
+++ pokemon.py
@@ -0,0 +1,99 @@
+import requests
+import json
+import datetime
+import pause
+import time
+import math
+import numpy as np
+import os
+import random
+
+header = {
+            "Authorization": "OTIwOTUwMTE3MjI4NDkwNzky.YdTofw.3dot1Af3IkNxt36P2RUOp0Hqcc4",
+            "Content-Type": "application/json",
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36",
+    }
+
+def init():
+    token = os.environ['Token']
+    if not token:
+        raise Exception('empty token')
+
+    header['Authorization'] = token
+
+def sendMessage(channel_id,message): 
+    msg = {
+            "content": message
+    }
+    
+    url = "https://discord.com/api/v9/channels/{}/messages".format(channel_id)
+    try:
+        res = requests.post(url=url, headers=header, data=json.dumps(msg))
+
+    except:
+        pass
+
+    return res
+
+def delay(minutes):
+        now = datetime.datetime.now()
+        until_time = now + datetime.timedelta(minutes=minutes)
+        pause.until(until_time)
+
+def getMessages(channel): 
+    url = "https://discord.com/api/v9/channels/{}/messages".format(channel)
+    try:
+        res = requests.get(url=url, headers=header)
+
+        data = res.json()
+    except:
+        pass
+    return data
+
+
+def battle():
+    now = datetime.datetime.now()
+    until_time = now + datetime.timedelta(minutes=1)
+    
+    sendMessage(927963670330351686,'.route 1')
+    time.sleep(5)
+    # sendMessage(927963670330351686,'1')
+    message = getMessages(927963670330351686)
+
+    keep_attect = True
+
+    while keep_attect:
+        attect_method = random.choice(["1", '2'])
+        sendMessage(927963670330351686,attect_method)
+        time.sleep(5)
+        messages = getMessages(927963670330351686)
+        for message in messages:
+            name = ''
+            if  len(message['embeds']) > 0:     
+                try:
+                    name = message['embeds'][0]['author']['name']
+                except KeyError:
+                    # Key is not present
+                    pass
+            if name.find('Charmander') != -1:
+                break  
+
+            if  name == 'Wild battle has ended!':
+                keep_attect = False
+                print('battle ended')
+
+                break 
+
+    now = datetime.datetime.now()
+    print(now)
+    print(until_time)
+    if now < until_time:
+        print('need delay')
+        pause.until(until_time)
+        print('delay finish')
+             
+
+if __name__ == "__main__":
+    init()
+    while True:
+        battle()
